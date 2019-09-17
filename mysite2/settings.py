d6111fdb7f6bb9081a9666739d0e93ad3a2dfe34
michrodolfos <48481352+michrodolfos@users.noreply.github.com>
2019-09-17 14:35:54 +0300

    Update settings.py

--- mysite2/settings.py
+++ mysite2/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'k+-0b#ct%-xq9w%6aeh=^43yo*8t&vo-+8ufj1+t+om@-j^*zm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['rodolfos.eu.pythonanywhere.com']
 
 
 # Application definition
